af1fad2287ffd746e2b206d617853acee445fd5f
Trillion Zhao <iezhaoxy@163.com>
2019-07-05 12:22:14 +0800

    Increase reusability

--- 0005.py
+++ 0005.py
@@ -3,16 +3,28 @@
 你有一个目录
 装了很多照片
 把它们的尺寸变成都不大于 iPhone5 分辨率的大小
+0022/0025
+复用
+iPhone 6
+iPhone 6 Plus
 '''
 from PIL import Image
+import argparse
 import os
 
+ext = ['jpg', 'jpeg', 'png']
+iphone_size = {
+    'iphone5': [640, 1136],
+    'iphone6': [750, 1334],
+    'iphone6plus': [1242, 2208]
+}
+# 增加了复用性Use: python 0005.py -s iphone5/iphone6/...
 def reSize(filename, match_size):
     image = Image.open(filename)
     w, h = image.size
     mwidth, mheight = match_size[0], match_size[1]
     if w <= mwidth and h <= mheight:
-        print(filename, 'match with iphone5.')
+        print(filename, 'matched with %s.' % (args['size']))
         return 
     if 1.0*w/mwidth > 1.0*h/mheight:
         scale = 1.0*w/mwidth
@@ -24,11 +36,11 @@ def reSize(filename, match_size):
     new_im.close()
 
 if __name__ == "__main__":
-    ext = ['jpg', 'jpeg', 'png']
-    match_size = [640, 1130]
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-s", "--size", required=True, help="iphone's size")
+    args = vars(ap.parse_args())
     # 获得当前路径文件列表
     files = os.listdir(os.getcwd())
-
     for file in files:
         '''
         str.split(str="",num=string.count(str))[n]
@@ -37,5 +49,5 @@ if __name__ == "__main__":
         [n]:表示选取第n个分片
         '''
         if file.split('.')[-1] in ext:
-            reSize(file, match_size)
+            reSize(file, iphone_size[args['size']])
 
